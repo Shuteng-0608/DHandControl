9b223da500c00c97f0a4e22a47e0314010cc147b
Shuteng Wang <117995375+Shuteng-0608@users.noreply.github.com>
2025-06-05 14:29:30 +0800

    Add files via upload

--- DHandControl/scripts/main.py
+++ DHandControl/scripts/main.py
@@ -1,5 +1,6 @@
 import json
 import socket
+import time
 
 # List of ESP32 IP addresses
 ESP32_IPS = [
@@ -18,15 +19,25 @@ def send_udp_message(ip, port, message):
     sock.sendto(message.encode(), (ip, port))
     sock.close()
 
-def turn(ID):
-    ip = "192.168.4.5"
-    message = {
-        'Cmd': "Turn",
-    }
-    json_message = json.dumps(message)
-    send_udp_message(ip, UDP_PORT, json_message)
+# def turn(ID):
+#     ip = "192.168.4.5"
+#     message = {
+#         'Cmd': "Turn",
+#     }
+#     json_message = json.dumps(message)
+#     send_udp_message(ip, UDP_PORT, json_message)
 
 def servo_move(ID, position, time_in_ms):
+    """
+    Init Pos:
+        - [1] : *247* - 1000
+        - [2] : 649 - *500* - 131
+        - [3] : 426 - *500* - 575
+    :param ID:
+    :param position:
+    :param time_in_ms:
+    :return:
+    """
     ip = "192.168.4.5"
     message = {
         'Cmd': "ServoMove",
@@ -36,6 +47,18 @@ def servo_move(ID, position, time_in_ms):
     }
     json_message = json.dumps(message)
     send_udp_message(ip, UDP_PORT, json_message)
+
+def move_palms(ID_list, pos_list, time_list):
+    ip = "192.168.4.5"
+    message = {
+        'Cmd': "MovePalms",
+        'ID_list': ID_list,
+        'pos_list': pos_list,
+        'time_list': time_list
+    }
+    json_message = json.dumps(message)
+    send_udp_message(ip, UDP_PORT, json_message)
+
 
 def finger_move(ID, position):
     ip = "192.168.4.5"
@@ -47,27 +70,121 @@ def finger_move(ID, position):
     json_message = json.dumps(message)
     send_udp_message(ip, UDP_PORT, json_message)
 
-def move_fingers(IDNum, ID_list, pos_list):
+def move_fingers(ID_list, pos_list):
     ip = "192.168.4.5"
     message = {
         'Cmd': "MoveFingers",
-        'IDNum': IDNum,
         'ID_list': ID_list,
         'pos_list': pos_list
     }
     json_message = json.dumps(message)
     send_udp_message(ip, UDP_PORT, json_message)
 
+def clear_error(ID):
+    ip = "192.168.4.5"
+    message = {
+        'Cmd': "ClearError",
+        'ID': ID,
+    }
+    json_message = json.dumps(message)
+    send_udp_message(ip, UDP_PORT, json_message)
 
+def boxing():
+    move_fingers([2, 3, 4, 5], [2000, 2000, 2000, 2000])
+    time.sleep(0.4)
+    move_fingers([1], [690])
+    time.sleep(3)
+    free()
+
+def index2thumb():
+    move_fingers([1, 2], [600, 1330])
+    time.sleep(3)
+    free()
+
+def middle2thumb():
+    move_fingers([1, 3], [1130, 1700])
+    time.sleep(3)
+    free()
+
+def ring2thumb():
+    servo_move(2, 380, 500)
+    servo_move(1, 530, 500)
+    time.sleep(3)
+    move_fingers([1, 4], [820, 1360])
+    time.sleep(3)
+    free()
+
+def finger_free():
+    """
+        ==== ATTENTION ==== : 执行之前需要确认不会发生干涉
+    """
+    move_fingers([1, 2, 3, 4, 5], [0, 0, 0, 0, 0])
+    time.sleep(1)
+
+def hand_free():
+    """
+        ==== ATTENTION ==== : 执行之前需要确认不会发生干涉
+    """
+    servo_move(1, 247, 500)
+    servo_move(2, 500, 500)
+    servo_move(3, 500, 500)
+
+def free():
+    """
+    ==== ATTENTION ==== : 执行之前需要确认不会发生干涉
+    """
+    finger_free()
+    hand_free()
 
 
 if __name__ == "__main__":
-    # free()
-    # turn(1)
-    # servo_move(1, 500, 2000)
-    # finger_move(1,500)
-    move_fingers(5, [1,2,3,4,5], [100,200,300,400,500])
+    # clear_error(1)
+    # clear_error(2)
+    # clear_error(3)
+    # clear_error(4)
+    # clear_error(5)
+    # ================= #
 
+    # boxing()
+    # time.sleep(2)
+    # index2thumb()
+    # time.sleep(2)
+    # middle2thumb()
+    # time.sleep(2)
+    # ring2thumb()
+
+    # ================= #
+    # servo_move(2, 380, 1000)
+    # servo_move(1, 530, 1000)
+    # move_palms([1, 2], [530, 380], [1000, 1000])
+
+
+
+
+    # free() #
+    # turn(1)
+    # servo_move(1, 0, 2000)
+    # time.sleep(2)
+    # finger_move(5,500)
+    # finger_move(2,1000)
+    # finger_move(3, 1000)
+    # finger_move(4, 1000)
+    # finger_move(5, 1000)
+    # finger_move(2, 500)
+    # finger_move(3, 500)
+    # finger_move(4, 500)
+    # finger_move(5, 500)
+    # finger_move(4, 500)
+    # servo_move(1, 426, 100)
+
+    # move_fingers([2,3,4,5], [500,500,500,500])
+    # time.sleep(2)
+    # move_fingers([2,3,4,5], [1000,1000,1000,1000])
+    # time.sleep(2)
+    # move_fingers([2,3,4,5], [750,750,750,750])
+    # move_fingers([2],[0])
+    # move_fingers([4], [0])
+    pass
 
 
 
